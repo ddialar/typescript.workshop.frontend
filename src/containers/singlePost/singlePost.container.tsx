ab75e4b1744be672dc19127395bf7b70dec03f50
ddialar <ddialar@gmail.com>
2021-02-14 17:51:42 +0000

    feat(containers): Define the 'singlePost' container.

---
+++ src/containers/singlePost/singlePost.container.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react'
+import { Card } from 'semantic-ui-react'
+
+import { LikePostButton, CommentPostButton } from '@components'
+
+interface Props {
+    postId: string
+    postOwner: string
+    createdAt: string
+    body: string
+    likes: number
+    likedByUser: boolean
+    comments: number
+}
+
+export const SinglePost: FC<Props> = ({ postId, postOwner, createdAt, body, likes, likedByUser, comments }) => (
+  <Card fluid>
+    <Card.Content>
+      <Card.Header>{postOwner}</Card.Header>
+      <Card.Meta>{(new Date(createdAt)).toLocaleString()}</Card.Meta>
+      <Card.Description>{body}</Card.Description>
+    </Card.Content>
+    <hr />
+    <Card.Content extra>
+      <LikePostButton likes={likes} postId={postId} likedByUser={likedByUser} />
+      <CommentPostButton comments={comments} postId={postId} />
+    </Card.Content>
+  </Card>
+)
