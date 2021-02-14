429de2434b084e5513275578bf0bb12916f8e4af
ddialar <ddialar@gmail.com>
2021-02-14 22:29:05 +0000

    refactor(infrastructure.dataSources): Implement the 'createNewPost' method.

--- src/infrastructure/dataSources/post.dataSources.ts
+++ src/infrastructure/dataSources/post.dataSources.ts
@@ -1,5 +1,5 @@
-import { getAllPostsApi, getPostByIdApi } from '@api'
-import { mapErrorFromApi, mapPostsFromApiToBasicPosts, mapPostFromApiToFullPost } from '@mappers'
+import { createNewPostApi, getAllPostsApi, getPostByIdApi } from '@api'
+import { mapErrorFromApi, mapPostsFromApiToBasicPosts, mapPostFromApiToFullPost, mapPostFromApiToBasicPost } from '@mappers'
 
 export const getAllPosts = async () => {
   const result = await getAllPostsApi()
@@ -9,4 +9,9 @@ export const getAllPosts = async () => {
 export const getPostById = async (postId: string) => {
   const result = await getPostByIdApi(postId)
   return result.error ? mapErrorFromApi(result) : mapPostFromApiToFullPost(result)
+}
+
+export const createNewPost = async (postBody: string, token: string) => {
+  const result = await createNewPostApi(postBody, token)
+  return result.error ? mapErrorFromApi(result) : mapPostFromApiToBasicPost(result)
 }
