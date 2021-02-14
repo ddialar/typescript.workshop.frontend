0810a03ed19fb9e535309560bb10c724e319dea7
ddialar <ddialar@gmail.com>
2021-02-14 17:58:56 +0000

    feat(infrastructure.api): Define the API connection process.

---
+++ src/infrastructure/api/api.ts
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+export interface RequestArgs {
+  method: 'get' | 'post' | 'put' | 'delete'
+  url: string
+  body?: string | Record<string, string>
+  params?: Record<string, string>
+  token?: string
+  timeout?: number
+}
+
+export const runRequest = async ({ method, url, body, token, params, timeout = 5000 }: RequestArgs) => {
+  const requestConfig = {
+    method,
+    // TODO: The 'baseUrl' must be defined by an environment variable
+    baseUrl: 'http://localhost:3600',
+    url,
+    headers: {
+      'Content-type': 'application/json; charset=utf-8',
+      Authorization: token && `bearer ${token}`
+    },
+    params,
+    data: body && JSON.stringify(body),
+    timeout
+  }
+
+  try {
+    // NOTE: 'response' is AxiosResponse type.
+    const { data } = await axios(requestConfig)
+    return data
+  } catch (error) {
+    // NOTE: 'error' is AxiosError type.
+    const data = { error: true, message: error.message }
+    return data
+  }
+}
---
+++ src/infrastructure/api/index.ts
@@ -0,0 +1,2 @@
+export * from './authentication.api'
+export * from './post.api'
