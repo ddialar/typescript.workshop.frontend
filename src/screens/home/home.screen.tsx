ec70140b6a918e18b700e267f53d66438c4c3952
ddialar <ddialar@gmail.com>
2021-02-14 17:55:10 +0000

    feat(screens): Define the 'home' screen.

---
+++ src/screens/home/home.screen.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react'
+
+import { PostsListLayout } from '@layouts'
+
+export const HomeScreen: FC = () => <PostsListLayout />
---
+++ src/screens/index.ts
@@ -0,0 +1,4 @@
+export * from './home'
+export * from './singlePost'
+export * from './login'
+export * from './register'
