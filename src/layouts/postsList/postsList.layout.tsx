051f79fce5783d4a03c2a6c7986c678283705b22
ddialar <ddialar@gmail.com>
2021-02-14 22:33:25 +0000

    feat(layouts): Include the option to create new posts int the 'postsList' layout.

--- src/layouts/postsList/postsList.layout.tsx
+++ src/layouts/postsList/postsList.layout.tsx
@@ -1,14 +1,16 @@
-import React, { FC, useState, useEffect, ReactElement } from 'react'
+import React, { FC, useState, useEffect, ReactElement, useContext } from 'react'
 import { Grid } from 'semantic-ui-react'
 
 import { SECTION_TITLE, NO_POSTS_MESSAGE } from './postsList.constants'
 
+import { AppContext } from '@context'
 import { BasicPost } from '@types'
-import { ErrorComponent, Spinner } from '@components'
-import { PostCard } from '@containers'
+import { Spinner } from '@components'
+import { PostCard, PostForm } from '@containers'
 import { getAllPosts } from '@dataSources'
 
 export const PostsListLayout: FC = () => {
+  const { user } = useContext(AppContext)
   const [posts, setPosts] = useState<BasicPost[]>([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<string | null>(null)
@@ -29,9 +31,22 @@ export const PostsListLayout: FC = () => {
     })()
   }, [])
 
+  const addNewPost = (newPost: BasicPost): void => {
+    setPosts([...posts, newPost])
+  }
+
   const processPosts = (posts: BasicPost[]): ReactElement => {
     return (
       <Grid.Row columns={3}>
+        {
+          user?.token
+            ? (
+              <Grid.Column style={{ marginBottom: '20px' }}>
+                <PostForm token={user.token} addNewPost={addNewPost} setError={setError} />
+              </Grid.Column>
+            )
+            : null
+        }
         {
           posts.map(post => (
             <Grid.Column key={post.id} style={{ marginBottom: '20px' }}>
@@ -49,6 +64,7 @@ export const PostsListLayout: FC = () => {
         <Grid.Row>
           <Grid.Column textAlign="center">
             <h1>{posts ? SECTION_TITLE : NO_POSTS_MESSAGE}</h1>
+            {error ? (<div className="ui error message">{error}</div>) : null}
           </Grid.Column>
         </Grid.Row>
         {posts ? processPosts(posts) : null}
@@ -57,5 +73,5 @@ export const PostsListLayout: FC = () => {
     </>
   )
 
-  return (error && (<ErrorComponent message={error} />)) || buildPostsList()
+  return buildPostsList()
 }
