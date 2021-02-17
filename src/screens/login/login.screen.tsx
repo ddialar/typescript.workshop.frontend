f0b98b5fff93273d6b39a3999ac7c7960555b184
ddialar <ddialar@gmail.com>
2021-02-17 15:41:28 +0000

    refactor(screens): Remove all non necessary React imports.

--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 
 import { LoginForm } from '@containers'
--- src/screens/register/register.screen.tsx
+++ src/screens/register/register.screen.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 
 import { RegisterForm } from '@containers'
 import { SingleForm } from '@layouts'
