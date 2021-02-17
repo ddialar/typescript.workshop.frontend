27bb6ca4d75a43b5d01a28768490430d05cfdc64
ddialar <ddialar@gmail.com>
2021-02-17 13:54:26 +0000

    feat(infrastructure.dataSources): Implement the 'getAllExtendedPosts' method.

--- src/infrastructure/dataSources/post.dataSources.ts
+++ src/infrastructure/dataSources/post.dataSources.ts
@@ -1,17 +1,22 @@
-import { createNewPostApi, getAllPostsApi, getPostByIdApi } from '@api'
+import { createNewPostApi, getAllExtendedPostsApi, getAllPostsApi, getPostByIdApi } from '@api'
 import { mapErrorFromApi, mapPostsFromApiToBasicPosts, mapPostFromApiToFullPost, mapPostFromApiToBasicPost } from '@mappers'
 
 export const getAllPosts = async () => {
   const result = await getAllPostsApi()
-  return result.error ? mapErrorFromApi(result) : mapPostsFromApiToBasicPosts(result)
+  return 'error' in result ? mapErrorFromApi(result) : mapPostsFromApiToBasicPosts(result)
+}
+
+export const getAllExtendedPosts = async (token: string) => {
+  const result = await getAllExtendedPostsApi(token)
+  return 'error' in result ? mapErrorFromApi(result) : mapPostsFromApiToBasicPosts(result)
 }
 
 export const getPostById = async (postId: string) => {
   const result = await getPostByIdApi(postId)
-  return result.error ? mapErrorFromApi(result) : mapPostFromApiToFullPost(result)
+  return result && 'error' in result ? mapErrorFromApi(result) : mapPostFromApiToFullPost(result)
 }
 
 export const createNewPost = async (postBody: string, token: string) => {
   const result = await createNewPostApi(postBody, token)
-  return result.error ? mapErrorFromApi(result) : mapPostFromApiToBasicPost(result)
+  return 'error' in result ? mapErrorFromApi(result) : mapPostFromApiToBasicPost(result)
 }
