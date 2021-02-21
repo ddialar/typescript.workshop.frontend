cf8e82be4ebe5b23476f757480c1034c3a5d7344
ddialar <ddialar@gmail.com>
2021-02-21 23:08:35 +0000

    feat(containers): Implement the post deletion button in the 'singlePost' container.

--- src/containers/singlePost/singlePost.container.tsx
+++ src/containers/singlePost/singlePost.container.tsx
@@ -1,29 +1,27 @@
 import { FC } from 'react'
 import { Card } from 'semantic-ui-react'
 
-import { LikePostButton, CommentPostButton } from '@components'
+import { LikePostButton, CommentPostButton, DeletePostButton } from '@components'
+import { FullPost } from '@types'
 
 interface Props {
-    postId: string
-    postOwner: string
-    createdAt: string
-    body: string
-    likes: number
-    likedByUser: boolean
-    comments: number
+  post: FullPost
+  token?: string
+  onDelete: () => void
 }
 
-export const SinglePost: FC<Props> = ({ postId, postOwner, createdAt, body, likes, likedByUser, comments }) => (
+export const SinglePost: FC<Props> = ({ post: { id, owner, createdAt, body, userIsOwner, userHasLiked, comments, likes }, token, onDelete }) => (
   <Card fluid>
     <Card.Content>
-      <Card.Header>{postOwner}</Card.Header>
+      <Card.Header>{`${owner.name} ${owner.surname}`}</Card.Header>
       <Card.Meta>{(new Date(createdAt)).toLocaleString()}</Card.Meta>
       <Card.Description>{body}</Card.Description>
     </Card.Content>
     <hr />
     <Card.Content extra>
-      <LikePostButton likes={likes} postId={postId} likedByUser={likedByUser} />
-      <CommentPostButton comments={comments} postId={postId} />
+      <LikePostButton likes={likes.length} postId={id} likedByUser={userHasLiked} />
+      <CommentPostButton comments={comments.length} postId={id} />
+      {token && userIsOwner ? <DeletePostButton postId={id} onClick={() => onDelete()} /> : null}
     </Card.Content>
   </Card>
 )
