01ea215b85afa88f9047dc0164eb9ee5a1fd714c
ddialar <ddialar@gmail.com>
2021-02-23 10:36:27 +0000

    feat(common.routes): Define the 'POSTS_LIKE_PATH' route.

--- src/common/routes.ts
+++ src/common/routes.ts
@@ -3,4 +3,5 @@ export const LOGIN_PATH = '/login'
 export const LOGOUT_PATH = '/logout'
 export const REGISTER_PATH = '/register'
 export const POSTS_PATH = '/posts'
-export const POSTS_EXTENDED_PATH = '/posts/ext'
+export const POSTS_EXTENDED_PATH = `${POSTS_PATH}/ext`
+export const POSTS_LIKE_PATH = `${POSTS_PATH}/like`
