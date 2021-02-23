04f5d3af990facb2b4fb3f2afbe9d65cfa779ba1
ddialar <ddialar@gmail.com>
2021-02-23 10:40:12 +0000

    feat(infrastructure.api): Define the 'likePostApi' and 'dislikePostApi' methods.

--- src/infrastructure/api/post.api.ts
+++ src/infrastructure/api/post.api.ts
@@ -1,5 +1,5 @@
 import { runRequest, RequestArgs } from './api'
-import { POSTS_EXTENDED_PATH, POSTS_PATH } from '@common'
+import { POSTS_EXTENDED_PATH, POSTS_LIKE_PATH, POSTS_PATH } from '@common'
 import { ApiPost, ApiError } from '@types'
 
 export const getAllPostsApi = async (): Promise<ApiPost[] | ApiError> => {
@@ -46,6 +46,28 @@ export const createNewPostApi = async (postBody: string, token: string): Promise
     url: `${POSTS_PATH}`,
     token,
     body: { postBody }
+  }
+
+  return await runRequest(requestParams)
+}
+
+export const likePostApi = async (postId: string, token: string): Promise<ApiPost | ApiError> => {
+  const requestParams: RequestArgs = {
+    method: 'post',
+    url: `${POSTS_LIKE_PATH}`,
+    token,
+    body: { postId }
+  }
+
+  return await runRequest(requestParams)
+}
+
+export const dislikePostApi = async (postId: string, token: string): Promise<ApiPost | ApiError> => {
+  const requestParams: RequestArgs = {
+    method: 'delete',
+    url: `${POSTS_LIKE_PATH}`,
+    token,
+    body: { postId }
   }
 
   return await runRequest(requestParams)
