726560408f865ef5750899d36d10de066850ea8f
ddialar <ddialar@gmail.com>
2021-02-28 13:11:52 +0000

    feat(common): Define the 'POSTS_COMMENT_PATH' route.

--- src/common/routes.ts
+++ src/common/routes.ts
@@ -4,5 +4,6 @@ export const LOGIN_PATH = '/login'
 export const LOGOUT_PATH = '/logout'
 export const REGISTER_PATH = '/register'
 export const POSTS_PATH = '/posts'
+export const POSTS_COMMENT_PATH = `${POSTS_PATH}/comment`
 export const POSTS_EXTENDED_PATH = `${POSTS_PATH}/ext`
 export const POSTS_LIKE_PATH = `${POSTS_PATH}/like`
