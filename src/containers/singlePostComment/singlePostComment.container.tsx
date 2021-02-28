4f5825be2e21ce488042c2e694e5dde045867671
ddialar <ddialar@gmail.com>
2021-02-28 18:32:29 +0000

    feat(containers): Include the 'DeleteButton' in the 'SinglePostComment' container.

--- src/containers/singlePostComment/singlePostComment.container.tsx
+++ src/containers/singlePostComment/singlePostComment.container.tsx
@@ -1,16 +1,25 @@
+import { DeleteButton } from '@components'
+import { PostComment } from '@types'
 import { FC } from 'react'
 import { Card } from 'semantic-ui-react'
 
 interface Props {
-    commentOwner: string
-    body: string
-    createdAt: string
+    comment: PostComment
+    token?: string
+    onDelete: () => void
 }
 
-export const SinglePostComment: FC<Props> = ({ commentOwner, body, createdAt }) => (
+export const SinglePostComment: FC<Props> = ({ comment: { owner: { name, surname }, createdAt, body, userIsOwner }, token, onDelete }) => (
   <Card fluid>
     <Card.Content>
-      <Card.Header>{commentOwner}</Card.Header>
+      {
+        token && userIsOwner
+          ? <DeleteButton
+            title="Delete post comment confirmation"
+            message="Do you really want to delete this post comment?"
+            onClick={() => onDelete()} />
+          : null}
+      <Card.Header>{`${name} ${surname}`}</Card.Header>
       <Card.Meta>{(new Date(createdAt)).toLocaleString()}</Card.Meta>
       <Card.Description>{body}</Card.Description>
     </Card.Content>
