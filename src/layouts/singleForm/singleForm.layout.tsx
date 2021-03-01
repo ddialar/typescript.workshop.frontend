c75b39b99d92efd29d24e1fb3b94e2c558d6b623
ddialar <ddialar@gmail.com>
2021-03-01 18:56:31 +0000

    refactor(layouts): Remove all layouts due to they are not needed right now.

--- src/layouts/singleForm/singleForm.layout.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-import './SingleForm.styles.css'
-
-export const SingleForm: FC = ({ children }) => (
-  <div className="single-form-container">
-    {children}
-  </div>
-)
