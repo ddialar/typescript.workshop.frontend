39d675e82b52dcc1d4c92a2ef5c763d8bc64e988
ddialar <ddialar@gmail.com>
2021-03-01 18:57:54 +0000

    refactor(screens): Replace the 'SigleForm' layout by the 'FormWrapper' component in the 'login' screen.

--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -1,8 +1,7 @@
 import { FC } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 
-import { LoginForm } from '@components'
-import { SingleForm } from '@layouts'
+import { FormWrapper, LoginForm } from '@components'
 import { HOME_PATH } from '@common'
 
-export const LoginScreen: FC<RouteComponentProps> = ({ history }) => <SingleForm><LoginForm callback={() => history.push(HOME_PATH)} /></SingleForm>
+export const LoginScreen: FC<RouteComponentProps> = ({ history }) => <FormWrapper><LoginForm callback={() => history.push(HOME_PATH)} /></FormWrapper>
