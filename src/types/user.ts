f6b0968a87164d6acbeb5cdbc09632caebf15806
ddialar <ddialar@gmail.com>
2021-03-06 22:39:10 +0000

    feat(types): Define the 'UserDomainModel' interface.

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,3 +1,18 @@
+export interface UserDomainModel {
+  id: string
+  username: string
+  password: string
+  email: string
+  name: string
+  surname: string
+  avatar: string
+  token: string
+  enabled: boolean
+  deleted: boolean
+  lastLoginAt: string
+  createdAt: string
+  updatedAt: string
+}
 export interface NewUserRegistryData {
   name: string
   surname: string
