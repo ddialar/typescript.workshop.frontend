22900a038d16e4b60f281e2c7fc3f40bf08e5dc1
ddialar <ddialar@gmail.com>
2021-03-07 08:46:24 +0000

    feat(validators): Implement the 'newPostSchema' and 'newPostCommentSchema'.

--- src/infrastructure/validators/validation.schemas.ts
+++ src/infrastructure/validators/validation.schemas.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi'
 
-import { username, password, confirmPassword, email, name, surname, avatar } from './validation.rules'
+import { username, password, confirmPassword, email, name, surname, avatar, postBody, commentBody } from './validation.rules'
 
 export const nameSchema = Joi.object({ name })
 export const surnameSchema = Joi.object({ surname })
@@ -9,3 +9,6 @@ export const avatarSchema = Joi.object({ avatar })
 export const usernameSchema = Joi.object({ username })
 export const passwordSchema = Joi.object({ password })
 export const confirmPasswordSchema = Joi.object({ confirmPassword })
+
+export const newPostSchema = Joi.object({ postBody })
+export const newPostSCommentchema = Joi.object({ commentBody })
