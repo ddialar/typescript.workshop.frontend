4fa35ba26aa1ed824f2c4710ce844d3a73bfed5a
ddialar <ddialar@gmail.com>
2021-03-07 15:11:34 +0000

    tests(App): Skip the integration test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import { App } from './App'
 
-test('renders learn react link', () => {
+// TODO Implement Cypres and create the integration testing suites.
+test.skip('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/Social Media Empty Page/i)
   expect(linkElement).toBeInTheDocument()
