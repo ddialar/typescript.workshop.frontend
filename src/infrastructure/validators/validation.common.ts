ce9e7f9012712bcbbfbf7d4891c7849434320ba4
ddialar <ddialar@gmail.com>
2021-03-07 13:37:55 +0000

    refactor(validators): Edit the 'formatValidationResult' in order to include the 'thereAreErrors' field.

--- src/infrastructure/validators/validation.common.ts
+++ src/infrastructure/validators/validation.common.ts
@@ -1,11 +1,13 @@
 import { ValidationResult } from 'joi'
 import { GENERIC_FIELD_ERROR_MESSAGE } from './validation.constants'
 
-export const formatValidationResult = <T>(rawValidationResults: ValidationResult[]): T =>
+export type FormatedValidation<T> = T & { thereAreErrors?: boolean }
+
+export const formatValidationResult = <T>(rawValidationResults: ValidationResult[]): FormatedValidation<T> =>
   rawValidationResults.reduce((result, validation) => {
     const [key] = Object.keys(validation.value)
 
     return validation.error
-      ? { ...result, [key]: GENERIC_FIELD_ERROR_MESSAGE }
+      ? { ...result, thereAreErrors: true, [key]: GENERIC_FIELD_ERROR_MESSAGE }
       : result
   }, {} as T)
