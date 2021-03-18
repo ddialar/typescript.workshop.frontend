3f27855ea966fb528d286ccea65862ed3de2ce23
ddialar <ddialar@gmail.com>
2021-03-18 21:35:02 +0000

    refactor(components): Edit the 'ErrorComponent' in order to apply SemanticUI styles.

--- src/view/components/presentationals/Error/Error.component.tsx
+++ src/view/components/presentationals/Error/Error.component.tsx
@@ -1,16 +1,16 @@
 import { FC } from 'react'
-import { Grid } from 'semantic-ui-react'
+// import { Grid } from 'semantic-ui-react'
 
 interface Props {
   message?: string
 }
 
-export const ErrorComponent: FC<Props> = ({ message = 'Oops! There was an error.' }) => (
-  <Grid>
-    <Grid.Row>
-      <Grid.Column textAlign="center" color="red">
-        <h1>{message}</h1>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-)
+export const ErrorComponent: FC<Props> = ({ message = 'Oops! There was an error.' }) =>
+  // <Grid>
+  //   <Grid.Row>
+  //     <Grid.Column textAlign="center" color="red">
+  //       <h1>{message}</h1>
+  //     </Grid.Column>
+  //   </Grid.Row>
+  // </Grid>
+  <div className="ui error message">{message}</div>
